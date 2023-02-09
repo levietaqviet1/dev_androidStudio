ce510d90406afe5e99536313ace2b06a8bd80fea
viet <levietaqviet2001@gmail.com>
2023-02-09 17:00:12 +0700

    x

---
+++ Androi/app/src/main/java/com/example/demo1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Bước 2 khai báo ánh xạ view
+//        Button button = findViewById(R.id.btnSubmit);
+////        TextView textView = findViewById(R.id.test);
+//        EditText editText1 = findViewById(R.id.txt1);
+//        EditText editText2 = findViewById(R.id.txt2);
+        //Bước 3 xử lý logic, sự kiện
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+}
